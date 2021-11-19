319329acc4b8f2eb07a45fde33fae59bb7e36761
nahuelcacer <nahue_cge@hotmail.com>
2021-11-19 14:42:26 -0300

    Agregue tabla con escribanos y tarjetas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import db from './configFirebase/configFirebase'
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+
 
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    
+    const Consulta = async () => {
+      const datos = []
+      const documentos = await getDocs(collection(db, "escribanosVisa"));
+      documentos.forEach(doc => {
+        datos.push(doc.data())
+      })
+      setData(datos)
+    }
+    Consulta();
+  }, [])
+
+    
+  
   const [escribano, setEscribano] = useState({})
   const hanledEscribano = (e) => {
     setEscribano({
@@ -17,12 +34,18 @@ function App() {
     addEscribano(escribano)
   }
   const addEscribano = async (escribano) => {
+
     const docRef = await addDoc(collection(db,"escribanosVisa"), {
       nombre:escribano.nombre,
-      tarjeta:escribano.tarjeta
+      tarjeta:escribano.tarjeta,
+      tipotarjeta:escribano.tipotarjeta,
+      ref:data.length + 1
+
     });
     console.log("Document written with ID: ", docRef.id);
+    /*AGREGAR ALGO PARA QUE ACTUALICE DATOS DE HOOK DATA*/
   }
+
   return (
     <div className="container">
       <div className="input-group mb-3">
@@ -30,10 +53,45 @@ function App() {
           <label className="form-label">Agrega un escribano</label>
           <input onChange={(e)=>{hanledEscribano(e)}} className="form-control" name="nombre" placeholder="Nombre"></input>
           <input onChange={(e)=>{hanledEscribano(e)}} className="form-control" name="tarjeta" placeholder="Tarjeta"></input>
-          <button className="btn btn-primary">Agregar</button>
+          <input onChange={(e)=>{hanledEscribano(e)}} className="form-control" name="tipotarjeta" placeholder="Tipo de tarjeta"></input>
+
+          <button className="btn btn-primary mt-3">Agregar</button>
         </form>
       </div>
+      <div>
+        <div>PARAMETROS DE DEBITO AUTOMATICO</div>
+      </div>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Ref</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Tarjeta</th>
+              <th scope="col">Tipo</th>
 
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map(escribano => {
+                var tarjeta = escribano.tarjeta.match(/.{1,4}/g)
+                var cardNumber = tarjeta[0]+" "+tarjeta[1]+" "+tarjeta[2]+" "+tarjeta[3]
+                return (
+                  <tr key={escribano.id}>
+                    <td>{escribano.ref} </td>
+                    <td>{escribano.nombre}</td>
+                    <td>{cardNumber}</td>
+                    <td>{escribano.tipotarjeta}</td>
+
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+
+      </div>
       
       
     </div>
