86121ab5e302155043d58ecf118dad5674f7bc73
nahuelcacer <nahue_cge@hotmail.com>
2021-11-19 16:00:33 -0300

    terminada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import db from './configFirebase/configFirebase'
 import { collection, addDoc, getDocs } from "firebase/firestore";
+import { saveAs } from 'file-saver';
 
 
 
@@ -45,7 +46,55 @@ function App() {
     console.log("Document written with ID: ", docRef.id);
     /*AGREGAR ALGO PARA QUE ACTUALICE DATOS DE HOOK DATA*/
   }
+  const [periodo,setPeriodo] = useState("")
+  const [date,setDate] = useState("")
+  const Fecha = (fecha) => {
+    var fecha1 = fecha.split('-')
+    return fecha1[2] + fecha1[1] + fecha1[0].slice(2)
+  }
+  const [importe, setImporte] = useState([]);
+  const hanledImport = (e) => {
+    setImporte({
+      ...importe,
+      [e.target.name]: e.target.value
+    })
+  }
+  function Total() {
+    var total = []
+    for (let obj in importe) {
+      total.push(parseFloat(importe[obj]))
+    }
 
+    // importe.map(item=> {
+    //   total.push(parseFloat(item))
+    // })
+    var total1 = total.reduce((a, b) => a + b, 0)
+    var total2 = total1.toFixed(2).split(".");
+    var totalt = total2[0] + total2[1]
+    return totalt
+
+
+  }
+  const datos = () => {
+    let cadena = ""
+
+    data.map(item => {
+      var total = parseFloat(importe[item.ref])
+      var total1 = total.toFixed(2).split(".");
+      var totalt = total1[0] + total1[1]
+      cadena += "28341931" + "2" + item.tarjeta + item.ref.padStart(12, "0") + "00100101" + totalt.padStart(11, "0") + periodo+ " "  + " " + Fecha(date) + item.nombre.padEnd(40, " ") + " ".repeat(20) + "\n"
+    })
+    return cadena;
+  }
+
+  const CreateFile = () => {
+    var comercio = "28341931"
+    var header = comercio + "1" + Fecha(date) + data.length.toString().padStart(7, "0") + "0" + Total().padStart(14,"0") + " ".repeat(91) + "\n"
+
+    const file = new Blob([header + datos()], {type: 'text/plain'});
+    saveAs(file, "file.txt");
+  }
+  
   return (
     <div className="container">
       <div className="input-group mb-3">
@@ -58,8 +107,12 @@ function App() {
           <button className="btn btn-primary mt-3">Agregar</button>
         </form>
       </div>
-      <div>
+      <div className="row">
         <div>PARAMETROS DE DEBITO AUTOMATICO</div>
+        <div className="col-3"><input placeholder="MMAA" onChange={(e)=>{setPeriodo(e.target.value)}}></input></div>
+        <div className="col-3"><input type="date" onChange={(e)=>{setDate(e.target.value)}}></input></div>
+
+        <div className="col-3"><label>Importe total</label></div>
       </div>
       <div>
         <table className="table">
@@ -69,6 +122,8 @@ function App() {
               <th scope="col">Nombre</th>
               <th scope="col">Tarjeta</th>
               <th scope="col">Tipo</th>
+              <th scope="col">Importe</th>
+
 
             </tr>
           </thead>
@@ -83,6 +138,8 @@ function App() {
                     <td>{escribano.nombre}</td>
                     <td>{cardNumber}</td>
                     <td>{escribano.tipotarjeta}</td>
+                    <td><input name={escribano.ref} onChange={(e) => { hanledImport(e) }} placeholder="$"></input></td>
+
 
                   </tr>
                 )
@@ -92,6 +149,7 @@ function App() {
         </table>
 
       </div>
+      <button onClick={CreateFile} className="btn btn-primary">Crear archivo</button>
       
       
     </div>
